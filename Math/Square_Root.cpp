7ce8cc958ec15f9bcd9dd54a6a6272bad46a53b9
alpha-asb <153850077+alpha-asb@users.noreply.github.com>
2024-08-13 09:36:28 +0530

    Binary Search

--- Math/Square_Root.cpp
+++ Math/Square_Root.cpp
@@ -1,14 +1,12 @@
 class Solution {
   public:
     long long int floorSqrt(long long int n) {
-        int hi = n;
-        int lo = 1;
-        int ans = 0;
-        while(lo<hi){
+        long long int hi = n,lo = 1,ans = -1;
+        while(lo<=hi){
             long long int mid = lo + (hi-lo)/2;
             long long int root = mid*mid;
-            if(root == n)return root;
-            else if(root < n){lo = mid+1;ans = lo;}
+            if(root == n){return mid;}
+            else if(root < n){lo = mid+1;ans = mid;}
             else {hi = mid-1;}
         }
         return ans;
