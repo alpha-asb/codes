fc26224a6dc0e92a6a2387f3d08f21d59d984ab3
alpha-asb <153850077+alpha-asb@users.noreply.github.com>
2024-09-08 09:54:18 +0530

    use prev pointer to chop the the list

--- Linked-Lists/Split_Linkedlist_in_Parts.cpp
+++ Linked-Lists/Split_Linkedlist_in_Parts.cpp
@@ -1,1 +1,45 @@
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<ListNode*> splitListToParts(ListNode* head, int k) {
+        int len = 0;
+        ListNode*ptr = head;
+        while(ptr){
+            len++;
+            ptr = ptr->next;
+        }
+        vector<ListNode*>res(k,nullptr);
+        int div = len/k;
+        int rem = len%k;
+        int i = 0;
+        while((div || rem)  && head){
+           res[i] = head;
+           i++;
+           ListNode*prev = NULL;
+           if(div){
+              int ahead = div;
+              while(ahead--){
+                prev = head;
+                head = head->next;
+              }
+           }
+           if(rem){
+             prev = head;
+             head = head->next;
+             rem--;
+           }
+           prev->next = nullptr;
+        }
+        return res;
+    }
+};
