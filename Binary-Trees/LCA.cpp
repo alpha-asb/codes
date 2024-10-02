068f50ee81f45d7c2f5634429e4a83043619b4b1
alpha-asb <153850077+alpha-asb@users.noreply.github.com>
2024-10-02 23:10:56 +0530

    added recursive approach

--- Binary-Trees/LCA.cpp
+++ Binary-Trees/LCA.cpp
@@ -1,3 +1,4 @@
+// O(N) time O(H) space to store the path
 class Solution
 {
     public:
@@ -33,5 +34,28 @@ class Solution
               i++;
         }
         return v1[i-1];
+    }
+};
+
+
+// Alternate Approach : O(N) time and O(H) space 
+
+class Solution
+{
+    public:
+    //Function to return the lowest common ancestor in a Binary Tree.
+    Node* lca(Node* root ,int n1 ,int n2 )
+    {
+       if(root == NULL)return NULL;
+       if(root->data == n1 || root->data == n2){
+           return root;
+       }
+       Node*l = lca(root->left,n1,n2);
+       Node*r = lca(root->right,n1,n2);
+       
+       if(l && r){  // one val in left subtree and other in right
+           return root;
+       }
+       return (l != NULL)?l:r;
     }
 };
