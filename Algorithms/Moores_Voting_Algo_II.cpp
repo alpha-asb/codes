597631627251bade04ba1d8662d31853a6f3d526
alpha-asb <153850077+alpha-asb@users.noreply.github.com>
2024-10-03 12:21:25 +0530

    Update Moores_Voting_Algo_II.cpp

--- Algorithms/Moores_Voting_Algo_II.cpp
+++ Algorithms/Moores_Voting_Algo_II.cpp
@@ -1,1 +1,17 @@
-
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        int n = nums.size();
+        vector<int>res;
+        for(int n:nums){
+            mp[n]++;
+        }
+        for(auto it:mp){
+            if(it.second > n/3){
+                res.push_back(it.first);
+            }
+        }
+        return res;
+    }
+};
